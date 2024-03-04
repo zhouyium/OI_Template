2fdead1c9667f3752a9d12ba07ccde25810ba3ee
zhouyi <70430188+zhouyium@users.noreply.github.com>
2024-03-04 12:41:10 +0800

    增加 HashedString 类，单模数字符串哈希。

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # OI_Template
- OI相关的模板
+ OI相关的模板。计算写两个版本，一个数组版本，一个基于std::vector。
+
+# 目录结构
+OI_Template
+├───document_ch
+├───document_en
+├───src
+│   └───array
+│       └───string
+└───verify
+
+# 数组版本
+## 文件
+<table width="500px">
+<tr><th width="10%">文件名</th><th width="30%">功能</th><th width="40%">使用方法</th><th width="20%">模板题</th></tr>
+<tr>
+    <th>HashedString.hpp</th>
+    <th>单模数字符串哈希</th>
+    <th>
+        <pre align="left">
+            <code>
+HashedString<> hs1("abcd"); //缺省长度(5e5),缺省底数(13331),缺省模数(998244353)
+int val=hs1.get_hash();//获得"abcd"的哈希结果
+val=hs1.get_hash(2);//获得"bcd"的哈希结果
+val=hs2.get_hash(2,3);//获得"bc"的哈希结果
+            </code>
+        </pre>
+    </th>
+    <th></th>
+</tr>
+</table>
---
+++ src/array/string/HashedString.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <string>
+#include <cassert>
+
+/**
+ @brief 单模字符串哈希
+ @section HashedString
+ @section 用法
+ HashedString<> hs1; //缺省长度(5e5)
+ hs1.init("abcd");
+ int val=hs1.get_hash();//获得"abcd"的哈希结果
+ val=hs1.get_hash(2);//获得"bcd"的哈希结果
+ val=hs2.get_hash(2,3);//获得"bc"的哈希结果
+
+ HashedString<10010> hs2; //长度为10,000
+ HashedString<10010> hs3("ab");
+ HashedString<10010> hs4("ab",131,1e9+7);
+ ***/
+template<int N=(int)5e5+10>
+class HashedString {
+public:
+    HashedString() : _n(0) {}
+    HashedString(int base=13331, int mo=998244353){
+        _n = 0;
+        _base = base;
+        _mo = mo;
+    }
+    HashedString(const std::string &s, int base=13331, int mo=998244353){
+        _n = 0;
+        _base = base;
+        _mo = mo;
+        calc(s);
+    }
+    void init(const std::string &s){
+        _n = s.size();
+        calc(s);
+    }
+    int get_hash(int l=1, int r=-1){
+        if (r<0){
+            r=_n;
+        }
+        assert(_n);
+        assert(1<=l&&l<=_n&&l<=r);
+        assert(1<=r&&r<=_n);
+        int val = normal(1ll*_hash[l-1]*_pow[r-l+1]);
+        val = normal(_hash[r]-val);
+        return val;
+    }
+private:
+    long long normal(int x){
+        assert(_mo>0);
+        return (x%_mo+_mo)%_mo;
+    }
+    void calc(const string &s) {
+        assert(_n);
+        _pow[0] = 1;
+        _hash[0] = 0;
+        for(int i=1;i<=_n;i++){
+            _pow[i] = normal(1ll*_pow[i-1]*_base);
+            _hash[i] = normal(1ll*_hash[i-1]*_base+s[i-1]);
+        }
+    }
+private:
+    int _n;//长度
+    int _base;//底
+    int _mo;//模
+    int _pow[N];
+    int _hash[N];
+};
