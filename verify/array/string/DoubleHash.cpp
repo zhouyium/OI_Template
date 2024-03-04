7d91491107d5c0e962dc7c8ef112ffe9e97deece
zhouyi <70430188+zhouyium@users.noreply.github.com>
2024-03-04 13:43:04 +0800

    增加双模数哈希验证代码。

--- verify/array/string/DoubleHash.cpp
+++ verify/array/string/DoubleHash.cpp
@@ -1,22 +1,15 @@
-#pragma once
+/***
+https://www.luogu.com.cn/problem/P3370
+http://47.110.135.197/problem.php?id=8710
+***/
+#include <bits/stdc++.h>
+using namespace std;
+using LL=long long;
+using PLL=pair<LL,LL>;
 
 #include <string>
 #include <cassert>
 
-/**
- @brief 单模字符串哈希
- @section HashedString
- @section 用法
- HashedString<> hs1; //缺省长度(5e5)
- hs1.init("abcd");
- int val=hs1.get_hash();//获得"abcd"的哈希结果
- val=hs1.get_hash(2);//获得"bcd"的哈希结果
- val=hs2.get_hash(2,3);//获得"bc"的哈希结果
-
- HashedString<10010> hs2; //长度为10,000
- HashedString<10010> hs3("ab");
- HashedString<10010> hs4("ab",131,1e9+7);
- ***/
 template<int N=(int)5e5+10>
 class HashedString {
 public:
@@ -67,3 +60,44 @@ private:
     int _pow[N];
     int _hash[N];
 };
+
+const int N=1e4+10;
+HashedString<> hs1;
+HashedString<> hs2(133, 1e9+7);
+
+struct pairHash
+{
+    template<typename T,typename U>
+    size_t operator()(const pair<T, U> & p) const
+    {
+        return hash<T>()(p.first) ^ hash<U>()(p.second);
+    }
+
+    template<typename T, typename U>
+    bool operator() (const pair<T, U> & p1, const pair<T, U> & p2) const
+    {
+        return p1.first == p2.first && p1.second == p2.second;
+    }
+};
+
+unordered_map<PLL, LL, pairHash, pairHash> mp;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n,m;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        string s;
+        cin>>s;
+
+        hs1.init(s);
+        int v1 = hs1.get_hash();
+        hs2.init(s);
+        int v2 = hs2.get_hash();
+        mp[{int(v1), int(v2)}]++;
+    }
+    cout<<mp.size()<<"\n";
+    return 0;
+}
