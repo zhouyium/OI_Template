db2d69475a60961bf00dfdf6c023dc7b33f2a3ec
zhouyi <70430188+zhouyium@users.noreply.github.com>
2024-03-04 14:02:35 +0800

    增加 namespace laozhou。

--- src/array/string/HashedString.hpp
+++ src/array/string/HashedString.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cassert>
 
+namespace laozhou {
 /**
  @brief 单模字符串哈希
  @section HashedString
@@ -17,53 +18,54 @@
  HashedString<10010> hs3("ab");
  HashedString<10010> hs4("ab",131,1e9+7);
  ***/
-template<int N=(int)5e5+10>
-class HashedString {
-public:
-    HashedString(int base=13331, int mo=998244353){
-        _n = 0;
-        _base = base;
-        _mo = mo;
-    }
-    HashedString(const std::string &s, int base=13331, int mo=998244353){
-        _n = 0;
-        _base = base;
-        _mo = mo;
-        calc(s);
-    }
-    void init(const std::string &s){
-        _n = s.size();
-        calc(s);
-    }
-    int get_hash(int l=1, int r=-1){
-        if (r<0){
-            r=_n;
+    template<int N=(int)5e5+10>
+    class HashedString {
+    public:
+        HashedString(int base=13331, int mo=998244353){
+            _n = 0;
+            _base = base;
+            _mo = mo;
         }
-        assert(_n);
-        assert(1<=l&&l<=_n&&l<=r);
-        assert(1<=r&&r<=_n);
-        int val = normal(1ll*_hash[l-1]*_pow[r-l+1]);
-        val = normal(_hash[r]-val);
-        return val;
-    }
-private:
-    long long normal(int x){
-        assert(_mo>0);
-        return (x%_mo+_mo)%_mo;
-    }
-    void calc(const string &s) {
-        assert(_n);
-        _pow[0] = 1;
-        _hash[0] = 0;
-        for(int i=1;i<=_n;i++){
-            _pow[i] = normal(1ll*_pow[i-1]*_base);
-            _hash[i] = normal(1ll*_hash[i-1]*_base+s[i-1]);
+        HashedString(const std::string &s, int base=13331, int mo=998244353){
+            _n = 0;
+            _base = base;
+            _mo = mo;
+            calc(s);
         }
-    }
-private:
-    int _n;//长度
-    int _base;//底
-    int _mo;//模
-    int _pow[N];
-    int _hash[N];
-};
+        void init(const std::string &s){
+            _n = s.size();
+            calc(s);
+        }
+        int get_hash(int l=1, int r=-1){
+            if (r<0){
+                r=_n;
+            }
+            assert(_n);
+            assert(1<=l&&l<=_n&&l<=r);
+            assert(1<=r&&r<=_n);
+            int val = normal(1ll*_hash[l-1]*_pow[r-l+1]);
+            val = normal(_hash[r]-val);
+            return val;
+        }
+    private:
+        long long normal(int x){
+            assert(_mo>0);
+            return (x%_mo+_mo)%_mo;
+        }
+        void calc(const string &s) {
+            assert(_n);
+            _pow[0] = 1;
+            _hash[0] = 0;
+            for(int i=1;i<=_n;i++){
+                _pow[i] = normal(1ll*_pow[i-1]*_base);
+                _hash[i] = normal(1ll*_hash[i-1]*_base+s[i-1]);
+            }
+        }
+    private:
+        int _n;//长度
+        int _base;//底
+        int _mo;//模
+        int _pow[N];
+        int _hash[N];
+    };
+}
--- verify/array/string/DoubleHash.cpp
+++ verify/array/string/DoubleHash.cpp
@@ -7,63 +7,11 @@ using namespace std;
 using LL=long long;
 using PLL=pair<LL,LL>;
 
-#include <string>
-#include <cassert>
-
-template<int N=(int)5e5+10>
-class HashedString {
-public:
-    HashedString(int base=13331, int mo=998244353){
-        _n = 0;
-        _base = base;
-        _mo = mo;
-    }
-    HashedString(const std::string &s, int base=13331, int mo=998244353){
-        _n = 0;
-        _base = base;
-        _mo = mo;
-        calc(s);
-    }
-    void init(const std::string &s){
-        _n = s.size();
-        calc(s);
-    }
-    int get_hash(int l=1, int r=-1){
-        if (r<0){
-            r=_n;
-        }
-        assert(_n);
-        assert(1<=l&&l<=_n&&l<=r);
-        assert(1<=r&&r<=_n);
-        int val = normal(1ll*_hash[l-1]*_pow[r-l+1]);
-        val = normal(_hash[r]-val);
-        return val;
-    }
-private:
-    long long normal(int x){
-        assert(_mo>0);
-        return (x%_mo+_mo)%_mo;
-    }
-    void calc(const string &s) {
-        assert(_n);
-        _pow[0] = 1;
-        _hash[0] = 0;
-        for(int i=1;i<=_n;i++){
-            _pow[i] = normal(1ll*_pow[i-1]*_base);
-            _hash[i] = normal(1ll*_hash[i-1]*_base+s[i-1]);
-        }
-    }
-private:
-    int _n;//长度
-    int _base;//底
-    int _mo;//模
-    int _pow[N];
-    int _hash[N];
-};
+#include "array/string/HashedString.hpp"
 
 const int N=1e4+10;
-HashedString<> hs1;
-HashedString<> hs2(133, 1e9+7);
+laozhou::HashedString<> hs1;
+laozhou::HashedString<> hs2(133, 1e9+7);
 
 struct pairHash
 {
