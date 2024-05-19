64771af3be68621101111725261a66b9b91ca2ee
ZHOUYI-UM <91511540+ZHOUYI-UM@users.noreply.github.com>
2024-05-19 17:37:17 +0800

    完成并查集模板最终版本。

--- C++/dsu.cpp
+++ C++/dsu.cpp
@@ -1,133 +1,99 @@
-
-#include<bits/stdc++.h>
-using namespace std;
-using LL=long long;
-using PLL=pair<LL,LL>;
-const LL INF=0x3f3f3f3f3f3f3f3f;
-const double EPS=1e-10;
-const LL MO=1e9+7;
-
-const int N=5e5+10;
+#include <vector>
 
 //并查集节点定义
 template<typename T=long long>
 struct DSUNODE{
     int fa;//表示编号为i的节点父亲是谁。-1表示根节点
-    T w;//节点到父亲的权值
     //下面的内容根据题目添加
-	T minx;
-	T maxx;
+    T dis;//表示飞船与其所在列队头的距离
 };
 //并查集模板
 template<typename T=long long>
 struct DSU{
-	//
+    //
 private:
-	std::vector<DSUNODE<T>> val;
-	
+    std::vector<DSUNODE<T>> val;
+
 public:
-	DSU(int n=5e5){
+    DSU(int n=5e5){
         //初始化数据
-		for(int i=0;i<=n;i++){
-			val.push_back({-1,0,i,i});
-		}
-	}
+        for(int i=0;i<=n;i++){
+            val.push_back({-1,0});
+        }
+    }
 
     //找x的父亲
-	int find(int x){
-		if(val[x].fa<0){
-			return x;
-		}
-		val[x].fa=find(val[x].fa);
-		val[x].w+=val[x].w;//val[x].w原来是与t的相对距离，现在是相对root的距离
-		return val[x].fa;
-	}
-	
-	bool join(int x,int y,T c=0){
-		int fx=find(x);
-		int fy=find(y);
-		if(fx==fy){
-			return false;
-		}
-		val[fx].fa+=val[fy].fa;//合并集合大小
-		val[fy].fa=fx;
-		val[fy].w=val[x].w-val[y].w+c;
-		val[fx].minx=min(val[fx].minx,val[fy].minx);
-		val[fx].maxx=max(val[fx].maxx,val[fy].maxx);
-		return true;
-	}
-	
-	int size(int x){
-		int fx=find(x);
-		return -val[fx].fa;
-	}
-	
-	bool same(int x,int y){
-		int fx=find(x);
-		int fy=find(y);
-		return fx==fy;
-	}
-	
-	DSUNODE<T> node(int x){
-		int fx=find(x);
-		return val[fx];
-	}
+    int find(int x){
+        if(val[x].fa<0){
+            return x;
+        }
+        int t=val[x].fa;
+        val[x].fa=find(val[x].fa);
+        //维护到父亲的权值，每题不一样
+        val[x].dis+=val[t].dis;//val[x].w原来是与t的相对距离，现在是相对root的距离
+        return val[x].fa;
+    }
+
+    //x的父亲是y
+    bool join(int x,int y,T c=0){
+        int fx=find(x);
+        int fy=find(y);
+        if(fx==fy){
+            return false;
+        }
+        //按秩合并
+        //if(-val[fx].fa>-val[fy].fa){
+        //    swap(fx,fy);
+        //}
+
+        //处理权值变化根据题目来定
+        //val[fx].dis+=-val[fy].fa;
+        val[fx].dis=val[y].dis-val[x].dis+c;
+
+        //合并集合
+        val[fy].fa+=val[fx].fa;//合并集合大小
+        val[fx].fa=fy;
+        return true;
+    }
+
+    void add(int x, T c){
+        int fx=find(x);
+        val[fx].dis+=c;
+    }
+
+    int size(int x){
+        int fx=find(x);
+        return -val[fx].fa;
+    }
+
+    bool same(int x,int y){
+        int fx=find(x);
+        int fy=find(y);
+        return fx==fy;
+    }
+
+    DSUNODE<T> node(int x){
+        return val[x];
+    }
+
+    //查询两点之间距离，如果不是同一组，返回-1
+    //每题都有区别
+    T dis(int x, int y){
+        if(!same(x,y)){
+            return -1;
+        }
+        return abs(val[x].dis-val[y].dis);
+    }
 };
-
-void solve() {
-	int n,m;
-	cin>>n>>m;
-	DSU<> dsu(n);
-	for(int i=1;i<=m;i++){
-		string op;
-		cin>>op;
-		int x,y;
-		if(op=="union"){
-			cin>>x>>y;
-			dsu.join(x,y);
-		}else if(op=="get"){
-			cin>>x;
-			DSUNODE<long long> ret=dsu.node(x);
-			cout<<ret.minx<<" "<<ret.maxx<<" "<<-ret.fa<<"\n";
-		}
-	}
-}
-
-int main() {
-#if 1
-	//�ύ��OJ
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	
-	LL T=1;
-	//cin>>T;
-	while(T--) {
-		//cout<<i<<"\n";
-		solve();
-	}
-#else
-	int n=10;
-	for(int i=0;i<n;i++){
-		char in[16];
-		sprintf(in, "%02d.in", i);
-		char out[16];
-		sprintf(out, "%02d.out", i);
-		
-		freopen(in, "r", stdin);
-		freopen(out, "w", stdout);
-		
-		LL T=1;
-		//cin>>T;
-		while(T--) {
-			//cout<<i<<"\n";
-			solve();
-		}
-		
-		fclose(stdin);
-		fclose(stdout);
-	}
-#endif
-	
-	return 0;
-}
+/*
+ *使用方法
+ * 初始化
+ *     DSU<> dsu(n+10);//并查集大小为n+10.权值为long long
+ *     DSU<int> dsu(n+10);//并查集大小为n+10.权值为int
+ * 查询是否属于同一个集合
+ *     dsu.same(x,y);
+ * 合并
+ *     dsu.join(x,y,c);//c表示距离
+ * 获取距离
+ *     dsu.dis(x,y);
+*/
