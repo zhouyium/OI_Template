376f7cf7121d0d9f09d8c3bb540d8e717de316be
ZHOUYI-UM <91511540+ZHOUYI-UM@users.noreply.github.com>
2024-05-26 10:50:31 +0800

    利用数组版AC自动机，验证P5337。

--- C++/SegmentTree.cpp
+++ C++/SegmentTree.cpp
@@ -1,166 +1,105 @@
-<template T>
-class LazySegmentTreeWithSetUpdate{
+#include<vector>
+template<typename T=long long>
+struct SEGNODE{
+    int ls;//左儿子
+    int rs;//右儿子
+    T add;//Lazy tag;
+    T sum;//和
+};
+template<typename T=long long>
+class SEGMENTTREE{
 private:
-    int n=0;
-    int MYSIZE=0;
-    vector<T> tree,lazy,stamp;
-    vector<bool> pending;
-    vector<T> arr;
-    int tm=0;
+    std::vector<SEGNODE<T>> val;//保存数据
+
+private:
+    //将左右儿子合成一个新节点
+    void combine(SEGNODE &t, const SEGNODE &t1, const SEGNODE &t2){
+        t.sum=t1.sum+t2.sum;
+        t.ls =min(t1.ls, t2.ls);
+        t.rs =max(t1.rs, t2.rs);
+    }
+
+    void eval(SEGNODE &t, T add){
+        t.sum = t.sum+(t.rs-t.ls+1)*add;
+        if(t.ls==t.rs){
+            //leaf
+            t.add=0;
+        }else{
+            t.add+=add;//加上懒标记
+        }
+    }
+
+    //下推
+    void pushdown(int u){
+        if(val[u].add==0){
+            return;
+        }
+        //更新左子树
+        eval(val[val[u].ls], val[u].add);
+        //更新右儿子
+        eval(val[val[u].rs], val[u].add);
+        //清标记
+        val[u].add=0;
+    }
 public:
-    void init(int n){
-        this->n=n;
-        this->MYSIZE=4*n+10;
-        tree.assign(MYSIZE,0LL);
-        lazy.assign(MYSIZE,0LL);
-        arr.assign(MYSIZE,0LL);
-        stamp.assign(MYSIZE,0LL);
-        pending.assign(MYSIZE,false);
+    //根据数组创建树
+    SEGMENTTREE(const std::vector<T> &vt){
+        build(1,1,vt.size()+1,vt)
     }
-    void post_init(){
-        this->constructST();
-    }
-    LazySegmentTreeWithSetUpdate(vector<T> & v){
-        int n=v.size();
-        init(n);
-        for(int i=0;i<n;i++){
-            arr[i]=v[i];
+
+    void build(int u, int l, int r,const std::vector<T> &vt){
+        int ls=val[u].ls;
+        int rs=val[u].rs;
+        if(l==r){
+            eval(val[u], vt[l-1]);
+            return val[u];
         }
-        post_init();
+        int m=(l+r)>>1;
+        build(ls,l,m);
+        build(rs,m+1,r);
+        combine(val[u], val[ls], val[rs]);
     }
-    LazySegmentTreeWithSetUpdate(T v[],int n){
-        init(n);
-        for(int i=0;i<n;i++){
-            arr[i]=v[i];
+
+    SEGNODE query(int u, int ql, int qr){
+        int ls=val[u].ls;
+        int rs=val[u].rs;
+        if(ql<=ls && rs<=qr){
+            return val[u];
         }
-        post_init();
-    }
-    LazySegmentTreeWithSetUpdate(int n){
-        init(n);
-        post_init();
-    }
-    void updateRangeUtil(int si, int ss, int se, int us,
-                         int ue, int diff,int stamp_value)
-    {
-        if (pending[si])
-        {
-            tree[si] = (se-ss+1)*lazy[si];
-            if (ss != se)
-            {
-                if(stamp[si*2+1]<=stamp[si]){
-                    lazy[si*2 + 1]   = lazy[si];
-                    stamp[si*2 + 1]   = stamp[si];
-                    pending[si*2+1]=true;
-                }
-                if(stamp[si*2+2]<=stamp[si]){
-                    lazy[si*2 + 2]   = lazy[si];    
-                    stamp[si*2 + 2]   = stamp[si];
-                    pending[si*2+2]=true;
-                }
-            }
-            pending[si]=false;
+        pushdown(u);
+
+        NODE res, t1, t2;
+        int m=(l+r)>>1;
+        if(ql<=m){
+            t1= query(ls,ql,qr);
         }
-        if (ss>se || ss>ue || se<us)
-            return ;
-        if (ss>=us && se<=ue)
-        {
-            tree[si] = (se-ss+1)*diff;
-            if (ss != se)
-            {
-                if(stamp[si*2+1]<=stamp_value){
-                    lazy[si*2 + 1]   = diff;
-                    stamp[si*2 + 1]   = stamp_value;
-                    pending[si*2+1]=true;
-                }
-                if(stamp[si*2+2]<=stamp_value){
-                    lazy[si*2 + 2]   = diff;    
-                    stamp[si*2 + 2]   = stamp_value;
-                    pending[si*2+2]=true;
-                }
-            }
-            stamp[si]=stamp_value;
+        if(m<qr){
+            t2= query(rs,ql,qr);
+        }
+        combine(res, t1, t2);
+        return res;
+    }
+
+    //[ql,qr]+c
+    void update(int u, int ql, int qr, int c){
+        int ls=val[u].ls;
+        int rs=val[u].rs;
+        if(ql<=ls && rs<=qr){
+            eval(val[u], c);
             return;
         }
-        int mid = (ss+se)/2;
-        updateRangeUtil(si*2+1, ss, mid, us, ue, diff,stamp_value);
-        updateRangeUtil(si*2+2, mid+1, se, us, ue, diff,stamp_value);
-        tree[si] = tree[si*2+1] + tree[si*2+2];
-    }
-    void updateRange(int us, int ue, int diff)
-    {
-       ++tm;
-       updateRangeUtil(0, 0, n-1, us, ue, diff,tm);
-    }
-    void update(int us,int ue,int diff){
-        updateRange(us,ue,diff);
-    }
-    T getSumUtil(int ss, int se, int qs, int qe, int si)
-    {
-        if (pending[si])
-        {
-             tree[si] = (se-ss+1)*lazy[si];
-            if (ss != se)
-            {
-                if(stamp[si*2+1]<=stamp[si]){
-                    lazy[si*2+1] = lazy[si];
-                    stamp[si*2+1]=stamp[si]; 
-                    pending[si*2+1]=true;   
-                }
-                if(stamp[si*2+2]<=stamp[si]){
-                    lazy[si*2+2] = lazy[si];
-                    stamp[si*2+2]=stamp[si];    
-                    pending[si*2+2]=true;
-                }
-            }
-            pending[si]=false;
+        pushdown(u);
+        int m=(l+r)>>1;
+        if(ql<=m){
+            update(ls,ql,qr,c);
         }
-        if (ss>se || ss>qe || se<qs)
-            return 0;
-        if (ss>=qs && se<=qe)
-            return tree[si];
-        int mid = (ss + se)/2;
-        return getSumUtil(ss, mid, qs, qe, 2*si+1) +
-               getSumUtil(mid+1, se, qs, qe, 2*si+2);
-    }
-    T getSum(int qs, int qe)
-    {
-        if (qs < 0 || qe > n-1 || qs > qe)
-        {
-            printf("Invalid Input");
-            return -1;
+        if(m<qr){
+            update(rs,ql,qr,c);
         }
-        return getSumUtil(0, n-1, qs, qe, 0);
-    }
-    T query(int qs,int qe){
-        return getSum(qs,qe);
-    }
-    void constructSTUtil(int ss, int se, int si)
-    {
-        if (ss > se)
-            return ;
-        if (ss == se){
-            tree[si] = arr[ss];
-            return;
-        }
-        int mid = (ss + se)/2;
-        if(ss<=mid) constructSTUtil(ss, mid, si*2+1);
-        if(mid+1<=se) constructSTUtil(mid+1, se, si*2+2);
-        tree[si] = tree[si*2 + 1] + tree[si*2 + 2];
-    }
-    void constructST(){
-        constructSTUtil(0, n-1, 0);
-    }
-    static void how_to_use(){
-        vector<int> arr={1,2,3,4};
-        LazySegmentTreeWithSetUpdate<int> *mylst=new LazySegmentTreeWithSetUpdate(arr);
-        mylst->update(0,3,0);
-        mylst->update(2,3,2);
-        mylst->update(0,2,10);
-        int ans=mylst->query(1,2);
-        cout<<ans<<endl;
+        combine(val[u], val[ls], val[rs]);
     }
 };
- 
+
 signed main(){
     vector<int> arr={1,2,3,4};
     LazySegmentTreeWithSetUpdate *mylst=new LazySegmentTreeWithSetUpdate(arr);
---
+++ verify/array/string/LuoGu/P5337.cpp
@@ -0,0 +1,152 @@
+/*
+ * https://www.luogu.com.cn/problem/P5337
+ * http://47.110.135.197/problem.php?id=10188
+ */
+#include<bits/stdc++.h>
+using namespace std;
+using LL=long long;
+
+/*
+ * 根据题目说明
+ * N=2e5+10
+ * AC自动机+DFS
+*/
+const int N=2e5+10;
+//AC自动机数据结构
+//在Trie中增加了 fail 指针
+struct TRIENODE{
+    int son[28];//叶子
+    int fail;//AC自动机fail
+    int end;//以自己为终点字符串出现的次数
+    //缺省构造函数
+    TRIENODE(){
+        fail=end=0;
+        for(int i=0;i<26;i++){
+            son[i]=0;
+        }
+    }
+} tree[N];
+int idx=0;//Trie指针
+LL num[N];
+vector<int> adj[N];
+
+//初始化
+void init(){
+    //清空数据
+    for(int i=0;i<=idx;i++){
+        tree[i].fail=tree[i].end=0;
+        for(int j=0;j<26;j++){
+            tree[i].son[j]=0;
+        }
+    }
+    idx=0;
+}
+
+//Trie树中插入字符串s
+//和标准Trie一样
+int insert(const std::string &s, int id){
+    int p = 0;//插入位置
+    for (const auto &ch : s){
+        int c = ch - 'a';
+        if(tree[p].son[c]==0){
+            //儿子不存在，增加新节点
+            tree[p].son[c]=++idx;
+        }
+        p = tree[p].son[c];
+    }
+    tree[id].end=p;
+    return p;
+}
+
+//使用BFS构建AC自动机，构造回跳边和转移边
+//即生成nxt的值
+void build(){
+    std::queue<int> que;
+    //BFS初始状态
+    for(int i=0;i<26;i++){
+        if(tree[0].son[i]>0){
+            //tree[tree[0].son[i]].fail=0;//指向根节点
+            que.emplace(tree[0].son[i]);
+        }
+    }
+    while(que.size()){
+        int p=que.front();
+        que.pop();
+        for(int i=0;i<26;i++){
+            int v=tree[p].son[i];
+            if(v==0){
+                //儿子不存在
+                //爹自建转移边
+                tree[p].son[i]=tree[tree[p].fail].son[i];
+            }else{
+                //儿子存在
+                //爹帮儿子建回跳边
+                tree[v].fail=tree[tree[p].fail].son[i];
+                //儿子入队
+                que.emplace(v);
+            }
+        }
+    }
+}
+
+//查询主串
+int query(const std::string &s){
+    int p=0, res=0;
+    for(const char ch:s){
+        p=tree[p].son[ch-'a'];//转移
+        num[p]++;
+    }
+    return res;
+}
+
+void dfs(int u){
+    for(const auto &v:adj[u]){
+        if(v==u){
+            continue;
+        }
+        dfs(v);
+        num[u]+=num[v];
+    }
+}
+
+void solve(){
+    //初始化
+    int n;
+    cin>>n;
+    //初始化
+    init();
+    string s;
+    //模式串
+    for(int i=1;i<=n;i++){
+        cin>>s;
+        insert(s,i);
+    }
+    build();
+
+    //查询主串
+    cin>>s;
+    query(s);
+    for(int i=1;i<=idx;i++){
+        adj[tree[i].fail].push_back(i);
+    }
+    dfs(0);
+
+    for(int i=1;i<=n;i++){
+        cout<<num[tree[i].end]<<"\n";
+    }
+    cout<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int T=1;
+    //cin>>T;
+    for(int i=1;i<=T;i++){
+        //cout<<"Case "<<i<<": ";
+        solve();
+    }
+    return 0;
+}
