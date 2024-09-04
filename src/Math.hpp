d812cc56582143ff8b8129ccba7acca328b31b5c
ZHOUYI-UM <91511540+ZHOUYI-UM@users.noreply.github.com>
2024-09-04 23:08:13 +0800

    增加 norm() 函数。

--- src/Math.hpp
+++ src/Math.hpp
@@ -1,4 +1,16 @@
 #pragma once
+
+// assume -P <= x < 2P
+int norm(int x, int P=1e9+7) {
+    if (x < 0) {
+        x += P;
+    }
+    if (x >= P) {
+        x -= P;
+    }
+    return x;
+}
+
 //快速幂
 template<class T>
 constexpr T power(T a, int b) {
