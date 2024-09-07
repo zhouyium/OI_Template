87086d33b86e1e8bbc1636b9d5c81cbc9ece8d75
ZHOUYI-UM <91511540+ZHOUYI-UM@users.noreply.github.com>
2024-09-07 19:10:39 +0800

    增加Z函数

--- src/string.hpp
+++ src/string.hpp
@@ -26,3 +26,20 @@ std::vector<int> manacher(const std::string &s) {
     }
     return r;
 }
+
+//Z函数
+std::vector<int> zFunction(const std::string &s) {
+    int n = s.size();
+    std::vector<int> z(n + 1);
+    z[0] = n;
+    for (int i = 1, j = 1; i < n; i++) {
+        z[i] = std::max(0, std::min(j + z[j] - i, z[i - j]));
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if (i + z[i] > j + z[j]) {
+            j = i;
+        }
+    }
+    return z;
+}
