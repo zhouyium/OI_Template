7c814a102a975feae9edcf3adbfcb252e9cc9974
ZHOUYI-UM <91511540+ZHOUYI-UM@users.noreply.github.com>
2024-09-17 11:11:43 +0800

    修改并查集实现的细节。

--- src/DSU.hpp
+++ src/DSU.hpp
@@ -1,23 +1,32 @@
 #pragma once
 #include <vector>
 
+/**
+ * @brief 并查集模板
+ * @version 1.0.0
+ * @date 2024-9-17
+ * @author YiZHOU
+ * @copydetails 本模板参考金牌选手 JiangLY 的模板
+ * 下面的链接是 JiangLY 使用提交
+ * https://ac.nowcoder.com/acm/contest/view-submission?submissionId=63239142
+ */
 struct DSU {
-    std::vector<int> id;
-    std::vector<int> sz;
+    std::vector<int> fa;//父亲节点是谁
+    std::vector<int> sz;//以编号 i 为父亲的并查集大小
     DSU() {}
     DSU(int n) {
         init(n);
     }
-    void init(int n) {
-        id.resize(n);
-        std::iota(id.begin(), id.end(), 0);
+    vofa init(int n) {
+        fa.resize(n);
+        std::iota(fa.begin(), fa.end(), 0);
         sz.assign(n, 1);
     }
     int find(int x) {
-        if (id[x] != x) {
-            id[x] = find(id[x]);
+        if (fa[x] != x) {
+            fa[x] = find(fa[x]);
         }
-        return id[x];
+        return fa[x];
     }
     bool same(int x, int y) {
         return find(x) == find(y);
@@ -29,7 +38,7 @@ struct DSU {
             return false;
         }
         sz[x] += sz[y];
-        id[y] = x;
+        fa[y] = x;
         return true;
     }
     int size(int x) {
@@ -40,7 +49,7 @@ struct DSU {
 /*
  * How to use.
  * 1. define a DSU with n nodes.
- *  DSU dsu(n+1);//I like to use from 1 to n.
+ *  DSU dsu(n);//由于使用vector，题目节点编号为 1~n，使用的时候是 0~n-1
  *
  * 2. find the father of x
  *  fa = dsu.find(x);
