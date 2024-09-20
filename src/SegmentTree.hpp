8f9a449fa6fd1595c242c2549d6509f345bcf6c4
zhouyi <70430188+zhouyium@users.noreply.github.com>
2024-09-20 11:25:59 +0800

    普通线段树模板完成。

--- src/SegmentTree.hpp
+++ src/SegmentTree.hpp
@@ -1,111 +1,98 @@
 #pragma once
 
-namespace lz{
-    namespace SegmentTree{
-        //普通线段树
-        //以下部分需要根据题目修改
-        struct Info{
-            int sum;//总和
-            int mx;//最大值
-            int cnt_mx;//最大值的次数
-            int mn;//最小值
-            int cnt_mn;//最小值的次数
-            Info(){
-                sum=0;
-                cnt_mx=cnt_mn=1;
-                mx=-INF;
-                mn=INF;
-            }
-        };
-        Info merge(const Info &n1, const Info &n2) {
-            Info c;
-            c.sum=n1.sum+n2.sum;
-            if(n1.mx>n2.mx){
-                c.mx=n1.mx;
-                c.cnt_mx=n1.cnt_mx;
-            }else if(n1.mx<n2.mx){
-                c.mx=n2.mx;
-                c.cnt_mx=n2.cnt_mx;
-            }else{
-                c.mx=n2.mx;
-                c.cnt_mx=n1.cnt_mx+n2.cnt_mx;
-            }
-            if(n1.mn<n2.mn){
-                c.mn=n1.mn;
-                c.cnt_mn=n1.cnt_mn;
-            }else if(n1.mn>n2.mn){
-                c.mn=n2.mn;
-                c.cnt_mn=n2.cnt_mn;
-            }else{
-                c.mn=n2.mn;
-                c.cnt_mn=n1.cnt_mn+n2.cnt_mn;
-            }
-            return c;
-        }
-        //以上部分需要根据题目修改
-        int _n;
-        //保存数据，我们从下标 1 开始使用
-        std::vector<Info> info;
-        void push_up(int p){
-            assert(1<=p && p<=4*_n);
-            assert(1<=2*p && 2*p<=4*_n);
-            assert(1<=2*p+1 && 2*p+1<=4*_n);
-            info[p]=merge(info[2*p],info[2*p+1]);
-        }
-        //建立线段树
-        //ve从下标 0 开始
-        void init(const std::vector<Info> &ve){
-            //线段树要开4倍
-            _n=ve.size();
-            info.assign(4*_n+1, Info());
-            //assert(4*_n<N);
-            std::function<void(int, int, int)> build = [&](int p, int l, int r){
-                if(r==l){
-                    info[p]=ve[l-1];
-                    return;
-                }
-                int m=(l+r)>>1;
-                build(2*p,l,m);
-                build(2*p+1,m+1,r);
-                push_up(p);
-            };
-            build(1,1,_n);
-        }
-        //区域更新
-        void modify(int p,int l,int r,int x,const Info &v){
-            if(r==l){
-                info[p]=v;
+/**
+ * @brief 普通线段树
+ * @version 1.0.0
+ * @date 2024-9-20
+ * @author YiZHOU
+ * @copydetails 本模板参考金牌选手 JiangLY 的模板
+ * 下面的链接是 JiangLY 使用提交
+ *      https://codeforces.com/contest/1672/problem/H   https://codeforces.com/contest/1672/submission/154766851
+ * 下面是我自己的提交
+ *      题目                                              代码
+ *      https://www.spoj.com/problems/GSS1/             https://paste.ubuntu.com/p/8yrdw83zRN/
+ */
+#include<functional>
+#include<cassert>
+#include<vector>
+
+struct Info {
+    //to add 根据题目定义需要维护的数据
+    //本例我们维护：和，最大值
+    int sum;//和
+    int mx;//最大值
+    //缺省构造函数
+    Info(){
+        //to add
+        sum = 0;
+        mx  = -INF;
+    }
+    //带参数构造函数
+    Info(int val){
+        //to add
+        sum = mx =val;
+    }
+};
+Info operator+(Info a, Info b) {
+    Info c;
+    // 对 a（左儿子） 和 b（右儿子） 一通操作合成 c（父结点）
+    // to do
+    c.sum = a.sum + b.sum;
+    c.mx  = std::max(a.mx, b.mx);
+    return c;
+}
+
+//普通线段树
+template<class Info,
+        class Merge = std::plus<Info>>
+struct SegmentTree {
+    const int n;
+    const Merge merge;
+    std::vector<Info> info;
+    SegmentTree(int n) : n(n), merge(Merge()), info(4 << std::__lg(n)) {}
+    SegmentTree(std::vector<Info> init) : SegmentTree(init.size()) {
+        std::function<void(int, int, int)> build = [&](int p, int l, int r) {
+            if (r - l == 1) {
+                info[p] = init[l];
                 return;
             }
-            int m=(l+r)>>1;
-            if(x<=m){
-                modify(2*p,l,m,x,v);
-            }else{
-                modify(2*p+1,m+1,r,x,v);
-            }
-            push_up(p);
+            int m = (l + r) / 2;
+            build(2 * p, l, m);
+            build(2 * p + 1, m, r);
+            pull(p);
+        };
+        build(1, 0, n);
+    }
+    void pull(int p) {
+        info[p] = merge(info[2 * p], info[2 * p + 1]);
+    }
+    void modify(int p, int l, int r, int x, const Info &v) {
+        if (r - l == 1) {
+            info[p] = v;
+            return;
         }
-        //单点更新
-        void modify(int p,const Info &v){
-            modify(1,1,_n,p,v);
+        int m = (l + r) / 2;
+        if (x < m) {
+            modify(2 * p, l, m, x, v);
+        } else {
+            modify(2 * p + 1, m, r, x, v);
         }
-        //区域查询
-        //[l,r]线段树表示区间
-        //[x,y]查询的区间
-        Info query(int p,int l,int r,int x,int y){
-            if(l > y || r < x){
-                //范围外
-                return Info();
-            }
-            if(x<=l && r<=y){
-                return info[p];
-            }
-            int m=(l+r)>>1;
-            return merge(query(2*p,l,m,x,y),query(2*p+1,m+1,r,x,y));
+        pull(p);
+    }
+    void modify(int p, const Info &v) {
+        modify(1, 0, n, p, v);
+    }
+    Info rangeQuery(int p, int l, int r, int x, int y) {
+        if (l >= y || r <= x) {
+            return Info();
         }
-        //单点查询
-        Info query(int l,int r){
-            return query(1,1,_n,l,r);
+        if (l >= x && r <= y) {
+            return info[p];
         }
-    }//namespace SegmentTree
-}//namespace lz
+        int m = (l + r) / 2;
+        return merge(rangeQuery(2 * p, l, m, x, y), rangeQuery(2 * p + 1, m, r, x, y));
+    }
+    Info rangeQuery(int l, int r) {
+        return rangeQuery(1, 0, n, l, r);
+    }
+};
--- verify/SegmentTree.md
+++ verify/SegmentTree.md
@@ -10,20 +10,59 @@
 
 ## Usage
 
+### 特别注意
+
+本模板存储的线段树和标准线段树不一样。
+
+标准线段树的范围是 [1, n]。
+
+本模板线段树的范围是 [0, n)。即节点从 0 开始计算。
+
 ### 包含命名空间
 
 using namespace lz;
 
 ### 修改代码
 
+
+
 这个部分代码需要根据具体题目确定。主要工作如下。
 
 1. 根据题目要求，修改结构体 Info
 
-2. 实现 merge() 函数
+```C++
+struct Info {
+    //to add 根据题目定义需要维护的数据
+    //本例我们维护：和，最大值
+    int sum;//和
+    int mx;//最大值
+    //缺省构造函数
+    Info(){
+        //to add
+        sum = 0;
+        mx  = -INF;
+    }
+    //带参数构造函数
+    Info(int val){
+        //to add
+        sum = mx =val;
+    }
+};
+```
+
+2. 实现 merge() 函数或者重载 operator+
 
 ```C++
 Info merge(const Info &n1, const Info &n2);
+
+Info operator+(Info a, Info b) {
+    Info c;
+    // 对 a（左儿子） 和 b（右儿子） 一通操作合成 c（父结点）
+    // to do
+    c.sum = a.sum + b.sum;
+    c.mx  = std::max(a.mx, b.mx);
+    return c;
+}
 ```
 
 该函数用于将节点 p 的左儿子 2*p 和右儿子 2*p+1 合并称为一个新的 Info。
@@ -52,39 +91,51 @@ for(int i=1;i<=n;i++){
 
 ### 初始化线段树
 
+可以根据构造的 ```C++ vector<Info> ``` 来初始化线段树。
 ```C++
-SegmentTree::init(ve);
+SegmentTree<Info> sgt(ve);
+```
+
+可以初始化空线段树。
+```C++
+//本例初始化 100 个节点的线段树
+SegmentTree<Info> sgt(100);
 ```
 
 ### 单点修改数据
 
 ```C++
-int pos=3;
-Info c;
-c.sum=5;
-SegmentTree::modify(pos, c);
+Info val(5);
+sgt.modify(pos, val);
 ```
 
 ### 查询
 
+特别注意，查询的区间为 [l,r)
 ```C++
 int l=1;
 int r=3;
-Info res=SegmentTree::query(l, r);
+Info res=sgt.rangeQuery(l, r);
 ```
 
 ## 例题
 
 ### 基础题
 
-[创建线段树](https://paste.ubuntu.com/p/JQ5Jtp6Bs2/)
+[查询区域和](https://paste.ubuntu.com/p/3wzfybXvZG/)
 
-[单点更新](https://paste.ubuntu.com/p/KRncMTnnNv/)
+[查询区域最大值](https://paste.ubuntu.com/p/WdHzQr4vbg/)
 
-[查询区域和](https://paste.ubuntu.com/p/R4NYq2HcVf/)
+[查询区域最小值和出现次数](https://paste.ubuntu.com/p/VRHR9XWnSJ/)
 
-[查询区域最大值和出现次数](https://paste.ubuntu.com/p/mG7YtRQhFP/)
+[CF EDU A. Segment Tree for the Sum](https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/submission/281929346)
+
+[CF EDU B. Segment Tree for the Minimum](https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/submission/281930695)
+
+[CF EDU C. Number of Minimums on a Segment](https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/submission/281931340)
 
 ### 进阶题
+
+[SPOJ GSS1](https://paste.ubuntu.com/p/8yrdw83zRN/)
 
 [CF1696 G. Fishingprince Plays With Array Again](https://codeforces.com/contest/1696/submission/281310638)
